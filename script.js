677f86482b7e2a89fcb749fa0c5888e7ee1a87b1
Mkups1 <m.kupilik1@share.epsb.ca>
2022-03-16 13:23:16 -0600

    Almost done

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("1.")
+for (let n = 1; n <= 500; n++) {
+    console.log("I'm so happy")
+}
+
+console.log("2.")
+
+for (let m = 12; m <= 800; m += 4) {
+    console.log(m)
+}
+
+console.log("3.")
+
+for (let x = 11; x <= 55; x += 2) { 
+    console.log(x)
+}
+
+console.log("4.")
+
+let startingValue = 5
+let endingValue = 50
+let total = 46
+
+let sum = total * (startingValue + endingValue) / 2
+console.log(sum)
+
+console.log("5.")
+
+let 
